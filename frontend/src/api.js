db24e16e78393401bf920e68afea8146fd47708c
Nguyen Huynh Tran <tran222543@student.nctu.edu.vn>
2025-11-09 01:57:18 +0700

    Xóa thư mục frontend và database khỏi nhánh backend-auth

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = "http://localhost:3000"; // ⚠️ Đúng cổng backend của bạn
-
-export const getUsers = () => axios.get(`${API}/users`);
-export const createUser = (data) => axios.post(`${API}/users`, data);
-export const updateUser = (id, data) => axios.put(`${API}/users/${id}`, data);
-export const deleteUser = (id) => axios.delete(`${API}/users/${id}`);
